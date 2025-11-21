b947064e1d83ceb8ec99025b915c53b6b1465513
imprvhub <ivanflowtramites@gmail.com>
2025-11-21 18:20:12 -0300

    style: swap cyan and blue color definitions

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -5,8 +5,8 @@ const colors = {
     green: '\x1b[38;2;79;201;176m', // #4FC9B0
     yellow: '\x1b[38;2;220;220;170m', // #DCDCAA
     red: '\x1b[31m',
-    cyan: '\x1b[36m',
-    blue: '\x1b[38;2;156;220;255m', // #9CDCFF
+    cyan: '\x1b[38;2;156;220;255m', // #9CDCFF
+    blue: '\x1b[36m',
     pink: '\x1b[38;2;206;146;120m', // #CE9278
     dim: '\x1b[2m'
 };
